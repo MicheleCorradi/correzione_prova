a1247d0f9acb24dbd0288feadc45f6cc5cdc931a
MicheleCorradi <michelecorradik@gmail.com>
2021-05-24 17:26:40 +0200

    Ho creato un catalogo e due immobili

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-
+import pickle
 
 class Immobile():
     
-    def __init__(self,riferimento,proprietario, indirizzo, prezzo):
+    def __init__(self,riferimento,proprietario, indirizzo, prezzo,citta):
         self.riferimento = riferimento
         self.proprietario = proprietario
         self.indirizzo = indirizzo
         self.prezzo = prezzo 
+        self.citta = citta
 
     def modifica_prezzo(self,prezzo):
         self.prezzo = prezzo
@@ -44,7 +45,26 @@ class Catalogo():
         for immobile in self.immobili:
             immobile.stampa.immobile()
 
-        
+
+    def salva(self):
+        with open(self.database, 'wb') as file:
+            pickle.dump(self.immobili, file)
+
+    def carica(self):
+        with open(self.database, 'rb') as file:
+            self.immobili = pickle.load(file)
+
+
+generale = Catalogo("generale")
+
+casa1 = Immobile("1a",20000,"Lelio Campanile","Via Roma","Aversa")
+casa2 = Immobile("2a",70000,"Caio","Via dei Gracchi","Roma")
+
+generale.aggiungi_immobile(casa1)
+generale.aggiungi_immobile(casa2)
+
+
+#generale.cerca_immobile("Via Roma")
 
 
 
